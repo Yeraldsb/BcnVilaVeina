08b297444f695f0ec6b342d38453e84b0bb7de01
Nicole Uy <marvienicolebtw@hotmail.com>
2021-09-01 12:14:46 +0200

    fixing styles and html structure of Form/Enquestas

--- front/src/components/Form/FormEnquestas.jsx
+++ front/src/components/Form/FormEnquestas.jsx
@@ -26,40 +26,36 @@ export const FormEnquestas = () => {
                                         <div className={"form-datalabelpicker"}>
                                             <Form.Group>
                                                 <Form.Label className={"data"}> Data : </Form.Label>
-                                            </Form.Group>
-                                             <Form.Group className={"formdatepicker"}>
-                                                <CustomDatePicker
-                                                    className="form-control"
-                                                    dateFormat="dd/MM/yyyy"
-                                                    selected={datepick}
-                                                />
+                                                    </Form.Group>
+                                                    <Form.Group className={"formdatepicker"}>
+                                                <CustomDatePicker className="form-control" dateFormat="dd/MM/yyyy" selected={datepick} />
                                             </Form.Group>
                                         </div>
-                    <div>
-                        <Form.Group>
-                            <Form.Label> Vila Veïna : </Form.Label>
-                                <div>
-                                    <Form.Select>
-                                        <option></option>
-                                        <option>Badal</option>
-                                            <option>Carmel de dalt</option>
-                                                <option>Casc antic d'Horta</option>
-                                                    <option>Casc antic Les Corts</option>
-                                                        <option>Consell de Cent - Girona - Eixample</option>
-                                                            <option>El Camp de l'Arpa - Alchemika</option>
-                                                                <option>El Congrés i els Indians</option>
-                                                                    <option>Gotic</option>
-                                                                    <option>La Marina - Mare de Déu del Port</option>
-                                                                <option>La Trinidad Vella</option>
-                                                            <option>Maresme</option>
-                                                        <option>Prosperitat</option>
-                                                    <option>Provençals de Poblenou</option>
-                                                <option>Sant Gervasi de Cassoles</option>
-                                            <option>Vila de Gracia</option>
-                                        <option>Vilapicina i la Torre LLobeta - Cotxeres</option>
-                                    </Form.Select>
-                                </div>
-                            </Form.Group>
+                            <div>
+                            <Form.Group>
+                                <Form.Label> Vila Veïna : </Form.Label>
+                                    <div>
+                                        <Form.Select>
+                                            <option></option>
+                                            <option>Badal</option>
+                                                <option>Carmel de dalt</option>
+                                                    <option>Casc antic d'Horta</option>
+                                                        <option>Casc antic Les Corts</option>
+                                                            <option>Consell de Cent - Girona - Eixample</option>
+                                                                <option>El Camp de l'Arpa - Alchemika</option>
+                                                                    <option>El Congrés i els Indians</option>
+                                                                        <option>Gotic</option>
+                                                                        <option>La Marina - Mare de Déu del Port</option>
+                                                                    <option>La Trinidad Vella</option>
+                                                                <option>Maresme</option>
+                                                            <option>Prosperitat</option>
+                                                        <option>Provençals de Poblenou</option>
+                                                    <option>Sant Gervasi de Cassoles</option>
+                                                <option>Vila de Gracia</option>
+                                            <option>Vilapicina i la Torre LLobeta - Cotxeres</option>
+                                        </Form.Select>
+                                    </div>
+                                </Form.Group>
 
                             <Form.Group>
                                 <Form.Label> Gènere </Form.Label>
@@ -91,10 +87,10 @@ export const FormEnquestas = () => {
                                         </Form.Select>
                                      </div>
                             </Form.Group>
-                            <div className={"formEdatNaixement"}>
+                            <div className={"form-age-origin"}>
                                 <Form.Group>
-                                    <Form.Label> Edat </Form.Label>
-                                         <Form.Select>
+                                    <Form.Label className={"age"}> Edat </Form.Label>
+                                         <Form.Select className={"age"}>
                                                  <option></option>
                                                      <option>-18 anys</option>
                                                          <option>De 18 a 24 anys</option>
@@ -108,38 +104,37 @@ export const FormEnquestas = () => {
                                                  <option>NS/NC</option>
                                          </Form.Select>
                                 </Form.Group>
+                                <Form.Group className={"llocdenaixement"}>
+                                    <Form.Label className={"origin"}> Lloc de naixement </Form.Label>
+                                        <Form.Select className={"origin"}>
+                                                <option></option>
+                                                    <option>Catalunya</option>
+                                                     <option>Resta de l'Estat</option>
+                                                        <option>Unión Europea</option>
+                                                            <option>Resta d'Europa</option>
+                                                                <option>Asia</option>
+                                                                    <option>Àfrica del Nord</option>
+                                                              <option>Resta d'Àfrica</option>
+                                                            <option>América del Sud i Central</option>
+                                                        <option>América del Nord</option>
+                                                    <option>Oceania</option>
+                                                <option>NS/NC</option>
+                                        </Form.Select>
+                                </Form.Group>
                             </div>
-                            <Form.Group className={"llocdenaixement"}>
-                                <Form.Label> Lloc de naixement </Form.Label>
-                                    <Form.Select>
-                                            <option></option>
-                                                <option>Catalunya</option>
-                                                 <option>Resta de l'Estat</option>
-                                                    <option>Unión Europea</option>
-                                                        <option>Resta d'Europa</option>
-                                                            <option>Asia</option>
-                                                                <option>Àfrica del Nord</option>
-                                                          <option>Resta d'Àfrica</option>
-                                                        <option>América del Sud i Central</option>
-                                                    <option>América del Nord</option>
-                                                <option>Oceania</option>
-                                            <option>NS/NC</option>
-                                    </Form.Select>
-                            </Form.Group>
                                 <Form.Group>
                                     <Form.Label> Àmbits Temàtiques </Form.Label>
-                                        <div className={"checkboxAmbit"}>
-                                        <input type="checkbox" id="ambitbox" className="ambitbox" name="AmbitTem"
-                                            value="AmbitTem"/>
-                                             <p>Pendent de desenvolupar</p>
+                                        <div className={"form-ambits-theme"}>
+                                        <input type="checkbox" className="ambit-box" />
+                                             <p className={"ambit-p"}>Pendent de desenvolupar</p>
                                         </div>
                                 </Form.Group>
                      </div>
                 </div>
             </div>
-            <div className={"columna2"}>
-                <div className={"columna2-items"}>
-                    <Form.Group className="grupo1">
+            <div className={"form-column-2"}>
+                <div className={"form-column-2-content"}>
+                    <Form.Group>
                         <Form.Label> Consulta de títol: </Form.Label>
                             <br/>
                             <Form.Select>
@@ -153,7 +148,7 @@ export const FormEnquestas = () => {
                                 <option> NS / NC</option>
                             </Form.Select>
                     </Form.Group>
-                    <Form.Group className="grupo2">
+                    <Form.Group >
                     <Form.Label> Motiu cures o suport: </Form.Label>
                         <Form.Select>
                             <option></option>
@@ -166,7 +161,7 @@ export const FormEnquestas = () => {
                             <option>NS/NC</option>
                         </Form.Select>
                     </Form.Group>
-                    <Form.Group className="grupo3">
+                    <Form.Group>
                         <Form.Label> Situació Jurídica: </Form.Label>
                             <div>
                                 <Form.Select>
@@ -181,7 +176,7 @@ export const FormEnquestas = () => {
                                 </Form.Select>
                             </div>
                     </Form.Group>
-                    <Form.Group className="grupo4">
+                    <Form.Group >
                         <Form.Label> Serveis i Tràmits: </Form.Label>
                             <div>
                                 <Form.Select>
@@ -195,7 +190,7 @@ export const FormEnquestas = () => {
                                 </Form.Select>
                             </div>
                     </Form.Group>
-                    <Form.Group className="grupo5">
+                    <Form.Group>
                         <Form.Label> Nivell d'estudis: </Form.Label>
                             <div>
                                 <Form.Select>
@@ -209,7 +204,7 @@ export const FormEnquestas = () => {
                                 </Form.Select>
                             </div>
                     </Form.Group>
-                    <Form.Group className="grupo6">
+                    <Form.Group >
                         <Form.Label> Situació Laboral: </Form.Label>
                             <div>
                                 <Form.Select>
@@ -225,7 +220,7 @@ export const FormEnquestas = () => {
                                 </Form.Select>
                             </div>
                     </Form.Group>
-                        <Form.Group className="grupo7">
+                        <Form.Group>
                             <Form.Label> Com ens ha conegut: </Form.Label>
                                 <div>
                                     <Form.Select>
