d4c85085638d5cb011a41442bdde1dcf7ffc1164
pamela salazar <you@example.com>
2021-09-03 11:29:52 +0200

    Cambios en el app

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,8 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import TablaEncuestadores from "./components/registerEncuestadores/TablaEncuestadores";
-import Estadisticas from "./components/estadisticas/Estadisticas";
-import {RegisterForm} from "./components/registerForm/RegisterForm";
+
 import {PaginaHome} from "./components/welcomePage/PaginaHome";
 import {useState} from "react";
-import {Home} from "@material-ui/icons";
 import AppAdmin from "./AppAdmin";
 import AppEncuestador from "./AppEncuestador";
-import {FormEnquestas} from "./components/Form/FormEnquestas";
-
 
 
 
@@ -28,34 +22,6 @@ function App() {
     }
     return <PaginaHome onSuccessfulLogin={enquestadorLogin} />
 
-
-
-  return (
-
-      <Router>
-          <Route exact path="/">
-              <PaginaHome/>
-          </Route>
-        <Switch>
-
-          <Route path="/tabla">
-            <TablaEncuestadores />
-          </Route>
-
-          <Route path="/enquesta">
-            <FormEnquestas />
-          </Route>
-          <Route path="/estadistiques">
-            <Estadisticas />
-          </Route>
-          <Route path="/register">
-            <RegisterForm />
-          </Route>
-
-        </Switch>
-
-      </Router>
-  );
 }
 
 export default App;
--- front/src/AppAdmin.js
+++ front/src/AppAdmin.js
@@ -1,10 +1,7 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import SideBarUser from "./components/sideBarUser/SideBarUser";
-import TablaEncuestadores from "./components/registerEncuestadores/TablaEncuestadores";
 import Estadisticas from "./components/estadisticas/Estadisticas";
 import {RegisterForm} from "./components/registerForm/RegisterForm";
-import {PaginaHome} from "./components/welcomePage/PaginaHome";
-import {FormEnquestas} from "./components/Form/FormEnquestas";
+import {FormEnquestas} from "./components/form/FormEnquestas";
 
 
 
