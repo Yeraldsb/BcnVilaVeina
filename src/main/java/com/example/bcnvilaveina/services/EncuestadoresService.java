6b2a0979acb1b65db0302b12d5f1304ce5d357c4
Sonia Mestre <mestre.sonia@gmail.com>
2021-09-08 14:28:12 +0200

    encuestadores services completed

--- src/main/java/com/example/bcnvilaveina/services/EncuestadoresService.java
+++ src/main/java/com/example/bcnvilaveina/services/EncuestadoresService.java
@@ -1,2 +1,28 @@
-package com.example.bcnvilaveina.services;public class EncuestadoresService {
+package com.example.bcnvilaveina.services;
+import com.example.bcnvilaveina.Model.Encuestadores;
+import com.example.bcnvilaveina.Repository.EncuestadoresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EncuestadoresService {
+
+    private EncuestadoresRepository encuestadoresRepository;
+
+    @Autowired
+    public EncuestadoresService(EncuestadoresRepository encuestadoresRepository) {
+        this.encuestadoresRepository = encuestadoresRepository;
+    }
+
+    public List<Encuestadores> allEncuestadores() {
+        return (List<Encuestadores>) encuestadoresRepository.findAll();
+    }
+
+    public Encuestadores getEncuestadores(String encuestadorId) {
+        return encuestadoresRepository.findById(encuestadorId).get();
+    }
+
 }
+
