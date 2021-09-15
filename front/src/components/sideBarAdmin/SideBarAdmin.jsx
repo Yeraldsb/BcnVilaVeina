6a9e0d1c6441c318a968c2e168d42f02ffd7f437
Sonia Mestre <mestre.sonia@gmail.com>
2021-09-15 11:58:39 +0200

    restyling adminnavbar & footer

--- front/src/components/sideBarAdmin/SideBarAdmin.jsx
+++ front/src/components/sideBarAdmin/SideBarAdmin.jsx
@@ -1,35 +1,35 @@
 import * as React from 'react';
 import "./sideBarAdmin.css"
 import vila from "../img/LogoVila.png";
-import green from "../img/listaVerde.png";
-import orange from "../img/estadisticaNaranja.png";
+import logoencuesta from "../img/logoencuestalila.jpeg";
+import logoestadisticas from "../img/logoestadisticaslila.jpeg";
 import {Footer} from "../footer/Footer";
-import altaBaja from "../img/altaBaja.png";
+import altaBaja from "../img/logoaltausuarilila.jpeg";
 import {Link} from "react-router-dom";
 
 
 
 export default function SideBarAdmin() {
     return (
-        <div className="sidebar">
-            <img src={vila} alt="" className="vila"/>
-            <div className="containerButton">
-                <Link to="/enquesta" className="link">
-                    <button><img src={green} alt="" className="logoSidebar"/>Nova enquesta</button>
+        <div className="sidebaradmin">
+            <img src={vila} alt="" className="logovilasideadmin"/>
+            <div className="containerButtonsideadmin">
+                <Link to="/enquesta" className="linktoenquesta">
+                    <button className="buttonsidebaradmin"><img src={logoencuesta} alt="" className="logobotonsidebar"/>NOVA ENQUESTA</button>
 
                 </Link>
                 <br/>
-                <Link to="/estadistiques" className="link">
-                    <button><img src={orange} alt="" className="logoSidebar"/>Estadístiques</button>
+                <Link to="/estadistiques" className="linktoenquesta">
+                    <button className="buttonsidebaradmin"><img src={logoestadisticas} alt="" className="logobotonsidebar"/>ESTADÍSTIQUES</button>
 
                 </Link>
                 <br/>
-                <Link to="/tablaEncuestadores" className="link">
-                    <button><img src={altaBaja} alt="" className="logoSidebar"/>Alta/Baixa Usuaris</button>
+                <Link to="/tablaEncuestadores" className="linktoenquesta">
+                    <button className="buttonsidebaradmin"><img src={altaBaja} alt="" className="logobotonsidebar"/>ALTA/BAIXA USUARIS</button>
 
                 </Link>
-                <Link to="/paginaHome" className="link">
-                    <button className="button-logout">Sortir</button>
+                <Link to="/paginaHome" className="linktoenquesta">
+                    <button className="button-logout">SORTIR</button>
                 </Link>
 
             </div>
