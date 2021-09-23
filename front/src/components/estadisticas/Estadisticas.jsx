6b2436e2171d10ddc41866aec4e4077c2f26bd3f
superco-art <daniela.trifu@gmail.com>
2021-09-23 12:42:18 +0200

    graficas edades terminada

--- front/src/components/estadisticas/Estadisticas.jsx
+++ front/src/components/estadisticas/Estadisticas.jsx
@@ -11,139 +11,147 @@ import {Bar} from "react-chartjs-2";
 import axios from "axios";
 import {Link} from "react-router-dom";
 
+function edadesDiferentes() {
+    return ["-18 anys", "De 18 a 24 anys", "De 25 a 34 anys", "De 35 a 44 anys", "De 45 a 54 anys", "De 55 a 64 anys", "De 65 a 74 anys", "De 75 a 84 anys", "De 85 y més", "NS/NC"]
+}
+
+function cuantasVecesSeRepite(edades, valoresPosibles) {
+    if (edades === null){
+        return []
+    }else {
+        return valoresPosibles.map(valorPosible =>
+            edades.filter(edad => edad === valorPosible).length)
+    }
+}
+
+
+
 export default function Estadisticas() {
 
 
     const [startDate, setStartDate] = useState(new Date("2021/08/31"));
     const [endDate, setEndDate] = useState(new Date("2021/08/31"));
-    const [edades, setEdades] = useState(null)
-    const [encuestas , setEncuestas ] = useState(null)
+    const [edades, setEdades] = useState([])
 
     React.useEffect(() => {
         axios.get('http://localhost:8080/all')
             .then((res) => {
-                setEdades(res.data)
-                setEncuestas(res.data)
+                setEdades(res.data.map(e => e.edat))
+
             }).catch(err => alert(err))
     }, []);
 
-    let respuestaEdades = []
-    if(edades){
-        respuestaEdades = edades.map( e => e.edat)
-    }
-    console.log(respuestaEdades)
-
 
     return (
-       <div className="mainContainer">
-           <div className="wrapperContainer">
-               <div className="dataPickerContainer">
-                   <p className="opcionFechas">Selecciona periode</p>
-                   <DatePicker
-                       selected={startDate}
-                       onCpnge={(date) => setStartDate(date)}
-                       selectsStart
-                       startDate={startDate}
-                       endDate={endDate}
-                   />
-                   <DatePicker
-                       selected={endDate}
-                       onCpnge={(date) => setEndDate(date)}
-                       selectsEnd
-                       startDate={startDate}
-                       endDate={endDate}
-                       minDate={startDate}
-                   />
-               </div>
-               <div className="buttonContainer">
-                   <button className="buttonEstadisticas botonesHoover">
-                       <img src={genere} alt="" className="iconoboton genere"/>
-                       <span className="span-genere">Gènere</span>
-                   </button>
+        <div className="mainContainer">
+            <div className="wrapperContainer">
+                <div className="dataPickerContainer">
+                    <p className="opcionFechas">Selecciona periode</p>
+                    <DatePicker
+                        selected={startDate}
+                        onCpnge={(date) => setStartDate(date)}
+                        selectsStart
+                        startDate={startDate}
+                        endDate={endDate}
+                    />
+                    <DatePicker
+                        selected={endDate}
+                        onCpnge={(date) => setEndDate(date)}
+                        selectsEnd
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={startDate}
+                    />
+                </div>
+                <div className="buttonContainer">
+                    <button className="buttonEstadisticas botonesHoover">
+                        <img src={genere} alt="" className="iconoboton genere"/>
+                        <span className="span-genere">Gènere</span>
+                    </button>
 
-                   <button className="buttonEstadisticas botonesHoover">
-                       <img src={edat} alt="" className="iconoboton genere "/>
-                       <span>Edat</span>
-                   </button>
+                    <button className="buttonEstadisticas botonesHoover">
+                        <img src={edat} alt="" className="iconoboton genere "/>
+                        <span>Edat</span>
+                    </button>
 
-                   <Link to="/barrasnacimiento">
-                   <button className="buttonEstadisticas botonesHoover">
-                       <img src={naixement} alt="" className="iconoboton"/>
-                       <span>Lloc naixement</span>
-                   </button>
-                   </Link>
+                    <Link to="/barrasnacimiento">
+                        <button className="buttonEstadisticas botonesHoover">
+                            <img src={naixement} alt="" className="iconoboton"/>
+                            <span>Lloc naixement</span>
+                        </button>
+                    </Link>
 
-                   <div className="buttonContainer second">
-                       <button className="buttonEstadisticas botonesHoover">
-                           <img src={enquestes} alt="" className="iconoboton encuesta"/>
-                           <span>Enquestes realitzades</span>
-                       </button>
+                    <div className="buttonContainer second">
+                        <button className="buttonEstadisticas botonesHoover">
+                            <img src={enquestes} alt="" className="iconoboton encuesta"/>
+                            <span>Enquestes realitzades</span>
+                        </button>
 
-                       <button className="buttonEstadisticas botonesHoover">
-                           <img src={consulta} alt="" className="iconoboton"/>
-                           <span>Consulta a títol</span>
-                       </button>
+                        <button className="buttonEstadisticas botonesHoover">
+                            <img src={consulta} alt="" className="iconoboton"/>
+                            <span>Consulta a títol</span>
+                        </button>
 
-                       <button className="buttonEstadisticas botonesHoover">
-                           <img src={suport} alt="" className="iconoboton"/>
-                           <span>Cures /suport</span>
-                       </button>
-                   </div>
-               </div>
+                        <button className="buttonEstadisticas botonesHoover">
+                            <img src={suport} alt="" className="iconoboton"/>
+                            <span>Cures /suport</span>
+                        </button>
+                    </div>
+                </div>
 
-           </div>
-           <div className="tablaTitleContainer">
-               <h3 className="tableTitle"> Tabla Title</h3>
-           </div>
-           <div className="graficaContainer">
+            </div>
+            <div className="tablaTitleContainer">
+                <h3 className="tableTitle"> Tabla Title</h3>
+            </div>
+            <div className="graficaContainer">
 
-               <Bar
-                   data={{
-                       labels: respuestaEdades,
-                       datasets: [{
-                           label: 'Gráfica rango de edad',
-                           data: [100, 200, 300, 400, 500, 600, 300, 400, 600, 150],
-                           backgroundColor: [
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                               'rgba(159,99,255,0.2)',
-                               'rgba(82,196,97,0.2)',
-                           ],
-                           borderColor: [
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                               'rgb(137,86,239)',
-                               'rgb(39,156,59)',
-                           ],
-                           borderWidth: 1
-                       }]
+                <Bar
+                    data={{
+                        labels: edadesDiferentes(),
+                        datasets: [{
+                            label: 'Gráfica rango de edad',
+                            data: cuantasVecesSeRepite(edades, edadesDiferentes()),
+                            backgroundColor: [
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                                'rgba(159,99,255,0.2)',
+                                'rgba(82,196,97,0.2)',
+                            ],
+                            borderColor: [
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                                'rgb(137,86,239)',
+                                'rgb(39,156,59)',
+                            ],
+                            borderWidth: 1
+                        }]
 
-                   }}
-                   height={180}
-                   width={300}
-                   options={{
-                       maintainAspectRatio: false
-                   }}
-               />
-           </div>
+                    }}
+                    height={180}
+                    width={300}
+                    options={{
+                        maintainAspectRatio: false
+                    }}
+                />
+            </div>
 
-       </div>
+        </div>
 
     )
 }
